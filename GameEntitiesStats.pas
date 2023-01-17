e96a8f6dbc72b391f352a55a9550ff8631944bc8
Hermosilla Reinaldo <ghost_cad@hotmail.com>
2023-01-17 01:27:14 -0300

    reduced Rabbit Hp from 5 to 3

--- GameEntitiesStats.pas
+++ GameEntitiesStats.pas
@@ -10,7 +10,7 @@ const
   nombres: array [1..2, 1..5] of string =
     (('Rabbit', 'Fox', 'Wolf', 'Bear', 'Shadow'), ('R', 'F', 'W', 'B', 'S'));
   estadisticas: array [1..4, 1..5] of integer =
-    ((0, 0, 1, 1, 1), (5, 8, 15, 30, 40), (1, 2, 4, 6, 8), (3, 4, 5, 6, 7));
+    ((0, 0, 1, 1, 1), (3, 8, 15, 30, 40), (1, 2, 4, 6, 8), (3, 4, 5, 6, 7));
   // aggro,         hp,           atk,       simbol
 
   nombresitems: array[1..2, 1..6] of string =
